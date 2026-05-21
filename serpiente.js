bcdba502eac72bf6d5f2e3634149fdc2a2d28529
alci19 <bytonalci@gmail.com>
2026-05-20 21:00:37 -0500

    Funciones para generar lineas horizontales y verticales

--- serpiente.js
+++ serpiente.js
@@ -1,25 +1,41 @@
+// 1. Capturamos el canvas y su contexto de dibujo
+const canvas = document.getElementById("canvasJuego");
+const ctx = canvas.getContext("2d");
+const TAMANIO_CELDA = 25; 
 
-    // 1. Capturamos el canvas y su contexto de dibujo
-    const canvas = document.getElementById("canvasJuego");
-    const ctx = canvas.getContext("2d");
+// Primera pintura del juego al cargar la página
+dibujarTodo();
+// =========================
+// FUNCIONES DE DIBUJO
+// =========================
 
+function limpiarCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
 
-    
+function dibujarTodo() {
+  limpiarCanvas();
+  dibujarTablero();
+}
 
-    // Primera pintura del juego al cargar la página
-    dibujarTodo();
+function dibujarTablero() {
+  ctx.strokeStyle = "#ffffff";
+//generanos las cuadriculas 
+  for (let x = 0; x <= canvas.width; x += TAMANIO_CELDA) {
+    ctx.beginPath();
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, canvas.height);
+    ctx.stroke();
+  }
 
-    // =========================
-    // FUNCIONES DE DIBUJO
-    // =========================
+  for (let y = 0; y <= canvas.height; y += TAMANIO_CELDA) {
+    ctx.beginPath();
+    ctx.moveTo(0, y);
+    ctx.lineTo(canvas.width, y);
+    ctx.stroke();
+  }
 
-    function limpiarCanvas() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-    }
-
-    function dibujarTodo() {
-      limpiarCanvas();
-    }
+}
 
 
 
